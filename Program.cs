ea2bbb1efb2b99ac41e90ba57f4d02b0d055fd07
Sergey86 <SolodovSV86@gmail.com>
2022-07-29 16:53:04 +0300

    I am looking for a component suitable for the conditions in the original array to add it to a new array

--- Program.cs
+++ Program.cs
@@ -3,3 +3,14 @@ string? origStr = Console.ReadLine();
 
 string[] origStrMass = origStr.Split(' ');
 string[] findWordsArr = new string[0];
+
+int j = 0;                                 
+for (int i = 0; i < origStrMass.Length; i++)  
+{
+    if (origStrMass[i].Length <= 3)           
+    {
+        Array.Resize(ref findWordsArr, findWordsArr.Length + 1); 
+        findWordsArr[j] = origStrMass[i];                        
+        j++;                                                   
+    }
+}   
